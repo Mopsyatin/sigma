339bb8169290b7a7e252e53c7aa3a827f27d851e
djuneyd <djuneyd2009@gmail.com>
2023-08-08 12:02:06 +0400

    pups

--- male/main.py
+++ male/main.py
@@ -2,23 +2,22 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def main():
     return render_template("index.html")
 
-@app.route("/<way>")
-def question1(way):
-    way = way
+@app.route("/1")
+def question1():
     return render_template("question_1.html")
 
-@app.route("/<way>")
-def question2(way):
-    way = way
+@app.route("/1/1")
+def question2():
     return render_template("question_2.html")
 
 @app.route("/correct")
 def correct():
-    return render_template("correct.html")
+    return render_template("correct_question_1.html")
 
 @app.route("/mistake")
 def mistake():
